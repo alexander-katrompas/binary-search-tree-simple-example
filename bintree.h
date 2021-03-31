669baefee3ec6417a6b8a5a312bb446ded801ba3
Alexander Katrompas <alexander.katrompas@austincc.edu>
2021-03-31 08:01:21 -0500

    created alternate constructor in place of 'dummy' addNode

--- bintree.h
+++ bintree.h
@@ -11,10 +11,11 @@ using std::string;
 
 class BinTree {
 public:
+    // this is the correct constructor
     BinTree();
     
-    //this is NOT a correct solution, you CANNOT use this assignments
-    void addNodes(int[], string[]);
+    //this is NOT a correct constructor, you CANNOT use this assignments
+    BinTree(int[], string[]);
     
     //this is a correct solution you can use in your assignment
     void displayInOrder();
@@ -28,4 +29,3 @@ private:
 };
 
 #endif /* BINTREE_H */
-
